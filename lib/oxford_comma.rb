4144ed669b94d4def8842d8a4ac2f3b0e0e878c4
Taulant Ibraj <tibraj94@gmail.com>
2020-03-28 18:37:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 1
     array.join
-elsif array.length == 2
+  elsif array.length == 2
     array.join(" and ")
   else
     [arr[0..-2].join(", "), arr.last].join(', and ')
